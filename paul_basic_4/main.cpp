e3caac3667073cca07586ea4982280739df248bd
yluo0203 <yluo0203@gmail.com>
2017-01-29 22:55:20 -0800

    Update main.cpp

--- paul_basic_4/main.cpp
+++ paul_basic_4/main.cpp
@@ -1,15 +1,30 @@
-//
-//  main.cpp
-//  paul_basic_4
-//
-//  Created by Tse Jen lu on 12/01/2017.
-//  Copyright © 2017 Tse Jen Lu. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+// Available for ONLY 3 characters
+int main() {
+    string str = "cat";
+    //cout << "Keying the word: " << endl;
+    //cin >> str;
+    
+    for (unsigned int i = 0; i <= str.length()-1; i++){
+        for (unsigned int j = 0; j <= str.length()-1; j++ ) {
+            for (unsigned int k = 0; k <= str.length()-1; k++) {
+                if( j != i && k != i && j != k) {
+                    string x = ( str.substr(j,1) );
+                    string y = ( str.substr(k,1) );
+                    swap(x,y);
+                    cout << str.substr(i,1) << y << x << endl;
+                }
+            }
+        }
+        
+    }
     return 0;
 }
